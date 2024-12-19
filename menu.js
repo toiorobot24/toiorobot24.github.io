2c8002dc198359b6765f0cc43c56f33d55ea8fb7
diidons <toiobros24@gmail.com>
2024-12-19 22:50:35 +0100

    V1

---
+++ menu.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        console.log(navLinks.classList); // Verifica se la classe 'active' è presente
+    });
+});
